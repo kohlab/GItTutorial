f22a6a0d5f346b75312f51e3ec16e0b78b6eb0db
Dylan Kohler <dylan@kohlab.com>
2019-11-08 10:00:38 -0800

    Initial Commit

---
+++ GItTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GItTutorial
+//
+//  Created by Dylan Kohler on 11/8/19.
+//  Copyright © 2019 RideHare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
