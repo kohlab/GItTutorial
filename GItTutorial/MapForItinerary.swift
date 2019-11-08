b62b899d3d87dbd03cca1c6784b429e4d8fbc5a1
Dylan Kohler <dylan@kohlab.com>
2019-11-08 10:16:15 -0800

    Add new map-for-itinerary feature

---
+++ GItTutorial/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GItTutorial
+//
+//  Created by Dylan Kohler on 11/8/19.
+//  Copyright © 2019 RideHare. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
